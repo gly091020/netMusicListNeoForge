0c459131f921c7e36ff5a4b3dc891d0dc73fa0d3
gly091020 <gulangyu1020@qq.com>
2025-08-08 21:19:35 +0800

    微调界面

--- src/main/java/com/gly091020/netMusicListNeoforge/client/MusicSelectionScreen.java
+++ src/main/java/com/gly091020/netMusicListNeoforge/client/MusicSelectionScreen.java
@@ -6,6 +6,7 @@ import com.gly091020.netMusicListNeoforge.PlayMode;
 import com.gly091020.netMusicListNeoforge.packet.DeleteMusicDataPacket;
 import com.gly091020.netMusicListNeoforge.packet.MoveMusicDataPacket;
 import com.gly091020.netMusicListNeoforge.packet.MusicListDataPacket;
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.math.Axis;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
@@ -345,6 +346,11 @@ public class MusicSelectionScreen extends Screen {
 
         @Override
         protected void renderListBackground(@NotNull GuiGraphics guiGraphics) {
+
+        }
+
+        @Override
+        protected void renderListSeparators(@NotNull GuiGraphics guiGraphics) {
 
         }
 
