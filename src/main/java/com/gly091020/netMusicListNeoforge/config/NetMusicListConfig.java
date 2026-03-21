86fda8c66e9832423d899236af760e4074bfb071
gly091020 <gulangyu1020@qq.com>
2026-03-22 00:55:32 +0800

    默认开启缓存

--- src/main/java/com/gly091020/netMusicListNeoforge/config/NetMusicListConfig.java
+++ src/main/java/com/gly091020/netMusicListNeoforge/config/NetMusicListConfig.java
@@ -13,7 +13,7 @@ public class NetMusicListConfig implements ConfigData {
     public int x = 10;
     public int y = 10;
     public int maxImportList = 300;
-    public boolean enableCache = false;
+    public boolean enableCache = true;
     public boolean globalCache = false;
 
     public float selectHudSize = 0.7f;
